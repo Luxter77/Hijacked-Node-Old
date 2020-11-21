0636d57508a4f8f34626256aeaac8d8d2e719e5a
Luxter77 <--global>
2020-11-21 03:09:20 -0300

    I am not very inteligent you know Like, just look at the kind of things I'm coding

--- hijackednode/funny.py
+++ hijackednode/funny.py
@@ -6,17 +6,20 @@ class NONONONONONONONONONONONO(NoneType):
         super.__init__(*args, **kargs) # Can you imagine how much does this line amuses me?
 
 class MyFriendThougtThisWouldBeFunnySoHereWeAre():
-    '''
-    TheEyeLoop said to me if I where to do something like this it would be funny.\n
-    I, of course, did instantly hiperfixated and did it.
-    '''
+    ''' TheEyeLoop said to me if I where to do something like this it would be funny.\n
+    I, of course, did instantly hiperfixated and did it.'''
     def __init__(self, *args, **kargs):
-        # You may be asking now:
-        #   Luxter, why the hug did you make this into a class instead of a recursive function
-        # I anwser:
-        #   You expect too much out of this dumpster fire of a module.
-        self.__call__(*args, **kargs)
-    
+        '''
+        You may be asking now:
+            Luxter, why the hug did you make this into a class instead of a recursive function
+        I anwser:
+            You expect too much out of this dumpster fire of a module.
+        '''
+        self.why_would_you_do_something_like_this_you_absolute_(*args, **kargs)
+
+    def __call__(*args, **kargs):
+        self.why_would_you_do_something_like_this_you_absolute_(*args, **kargs)
+
     def why_would_you_do_something_like_this_you_absolute_(self, unit: NoneType,
                                                            downstream: NONONONONONONONONONONONO
                                                            ) -> WhatTheHay:
