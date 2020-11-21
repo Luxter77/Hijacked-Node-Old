7aa23ecc31698e42eb10df410c9148c840b0dc2a
Luxter77 <--global>
2020-11-21 03:05:19 -0300

    TheEyeLoop forced me to do this (not really)

---
+++ hijackednode/funny.py
@@ -0,0 +1,30 @@
+from types import NoneType
+
+class WhatTheHay(set): pass
+class NONONONONONONONONONONONO(NoneType):
+    __init__(*args, **kargs):
+        super.__init__(*args, **kargs) # Can you imagine how much does this line amuses me?
+
+class MyFriendThougtThisWouldBeFunnySoHereWeAre():
+    '''
+    TheEyeLoop said to me if I where to do something like this it would be funny.\n
+    I, of course, did instantly hiperfixated and did it.
+    '''
+    def __init__(self, *args, **kargs):
+        # You may be asking now:
+        #   Luxter, why the hug did you make this into a class instead of a recursive function
+        # I anwser:
+        #   You expect too much out of this dumpster fire of a module.
+        self.__call__(*args, **kargs)
+    
+    def why_would_you_do_something_like_this_you_absolute_(self, unit: NoneType,
+                                                           downstream: NONONONONONONONONONONONO
+                                                           ) -> WhatTheHay:
+        for x in downstream:
+            for y in (x.__subclasses__()):
+                for z in y:
+                    _.add(z)
+        try:
+            _ = self.why_would_you_do_something_like_this_you_absolute_(downstream=_)
+        except:
+            return(_)
